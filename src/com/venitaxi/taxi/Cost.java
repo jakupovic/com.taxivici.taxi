4af102f18a5f0aa133ece00b5966a1b75fa46349
E J <jakupovic@gmail.com>
2010-09-29 01:49:34 -0400

    test eclipse git change

--- src/com/venitaxi/taxi/Cost.java
+++ src/com/venitaxi/taxi/Cost.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.widget.Toast;
  
 public class Cost extends Activity {
         @Override
